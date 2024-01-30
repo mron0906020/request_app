a4d8b778e654a6988e5def6708e6c624ca81bbce
mron <yuuki.kimura0303@gmail.com>
2024-01-30 21:52:36 +0900

    お天気API

--- request/views.py
+++ request/views.py
@@ -4,28 +4,36 @@ from .wetherapi import get_wether
 
 
 
+
+
+
+
+
 def index(request):
 
     return render(request, 'request/index.html')
 
 def detail(request):
-    if "post" in request.method.lower():
-        city_number=request.POST.get("location")
-        print("city_number->",city_number)        
-        weather=get_wether(city_number)
-        weather_title=weather["title"]
-        weather_date=weather["forecasts"]["date"]
-        weather_forecasts=weather["forecasts"]
-        weather_location=weather["location"]
-        weather_image=weather["image"]
-        
-        context={
-            "weather_title":weather_title,
-            "weather_date":weather_date,
-            "weather_forecasts":weather_forecasts,
-            "weather_location":weather_location,
-            "weather_image":weather_image,
-        }
-
+    city_number=request.POST["location"]
+    print("city_number->",city_number)        
+    weather=get_wether(city_number)
+    context={
+        "weather":weather
+    }
     return render(request, 'request/detail.html',context)
 
+
+        # weather_title=weather["title"]
+        # weather_forecasts=weather["forecasts"]
+        # weather_location=weather["location"]
+        # weather_image=weather["forecasts"][0]["image"]["url"]
+        # image = requests.get(weather_image).text
+
+
+        
+        # context={
+        #     "weather_title":weather_title,
+        #     "weather_forecasts":weather_forecasts,
+        #     "weather_location":weather_location,
+        #     "weather_image":image,
+        # }
--- request/wetherapi.py
+++ request/wetherapi.py
@@ -8,7 +8,39 @@ def get_wether(city_number):
    respons=requests.get(endpoint+"/city/"+city_number)
    print(respons)
    respons_json=respons.json()
-   return respons_json
+
+   weather_title=respons_json["title"]
+   weather_location=respons_json["location"]
+   weather_telop=respons_json["forecasts"][0]["telop"]
+   weather_deta=respons_json["forecasts"][0]["date"]
+   weather_image=respons_json["forecasts"][0]["image"]["url"]
+   image = requests.get(weather_image).text
+   weather_max=respons_json["forecasts"][0]["temperature"]["max"]["celsius"]
+   weather_min=respons_json["forecasts"][0]["temperature"]["min"]["celsius"]
+
+   context={
+            "weather_title":weather_title,
+            "weather_location":weather_location,
+            "weather_telop":weather_telop,
+            "weather_date":weather_deta,
+            "weather_image":image,
+            "weather_max":weather_max,
+            "weather_min":weather_min,
+        }
+   
+   return context
+
+   
+
+
+  
+  
+  
+  
+   # return respons_json
+
+
+
 # dict_deta=respons.json()
 # print(dict_deta["forecasts"][0]["date"]+dict_deta["forecasts"][0]["telop"])
 # print(dict_deta["forecasts"][0]["temperature"]["max"]["celsius"])
